26e6082963c11f10b00047e49ba49124e006f158
Trenton Broughton <trenton@devpie.com>
2014-02-13 16:55:51 -0500

    Renamed module to avoid conflicts

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import sys
 
 
 setup(
-    name = "dogstatsd-python",
+    name = "dogstatsd-txpython",
     version = "0.3.0",
     author = "Datadog, Inc.",
     author_email = "packages@datadoghq.com",
     description = "Python bindings to Datadog's API and a user-facing command line tool.",
-    py_modules=['statsd'],
+    py_modules=['txstatsd'],
     license = "BSD",
     keywords = "datadog data statsd metrics",
     url = "http://www.datadoghq.com",
