60d5b7e66aa2b7eaee14c492319bb7c7fde8f5d3
Lex Toumbourou <lextoumbourou@gmail.com>
2016-01-11 10:31:23 +1100

    Fix module name.

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import sys
 
 setup(
     name = "dogstatsd-txpython",
-    version = "0.3.0",
+    version = "0.3.1",
     author = "Datadog, Inc.",
     author_email = "packages@datadoghq.com",
     description = "Python bindings to Datadog's API and a user-facing command line tool.",
-    py_modules=['txstatsd'],
+    py_modules=['dogstatsd'],
     license = "BSD",
     keywords = "datadog data statsd metrics",
     url = "http://www.datadoghq.com",
